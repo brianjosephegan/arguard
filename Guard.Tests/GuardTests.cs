c641eda0800e8833456d2de630d052d2e9b8dee5
Brian Egan <brianjoseph.egan@gmail.com>
2020-12-24 16:00:54 -0800

    Initial Commit
    Add source and test project with implementation and tests for a single guard method.

---
+++ Guard.Tests/GuardTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Guard;
+using NUnit.Framework;
+
+namespace Guard.Tests
+{
+    /// <summary>
+    /// Unit tests for <see cref="Guard"/> class.
+    /// </summary>
+    [TestFixture]
+    public class GuardTests
+    {
+        [Test]
+        public void ArgumentNotNull_NullArgument()
+        {
+            string invalidString = null;
+
+            var ex = Assert.Throws<ArgumentNullException>(
+                () => Guard.ArgumentNotNull(invalidString, nameof(invalidString)));
+
+            StringAssert.Contains(nameof(invalidString), ex.ParamName);
+            StringAssert.Contains($"{nameof(invalidString)} cannot be null", ex.Message);
+        }
+
+        [Test]
+        public void ArgumentNotNull_NotNullArgument()
+        {
+            string validString = "This string is not null.";
+
+            Assert.DoesNotThrow(
+                () => Guard.ArgumentNotNull(validString, nameof(validString)));
+        }
+    }
+}
---
+++ Guard/Guard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guard
+{
+    /// <summary>
+    /// Class to provide a range of argument validation methods.
+    /// </summary>
+    public static class Guard
+    {
+        public static void ArgumentNotNull(object argumentValue, string argumentName)
+        {
+            if (argumentValue == null)
+            {
+                throw new ArgumentNullException(argumentName, $"{argumentName} cannot be null");
+            }
+        }
+    }
+}
