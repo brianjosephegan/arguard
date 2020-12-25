8e2a11cbc129a380ef5a0b0fe38c1ae238533215
Brian Egan <brianjoseph.egan@gmail.com>
2020-12-24 16:55:30 -0800

    Additional Guard Methods
    Add check for empty Guids and empty strings.

--- Guard.Tests/GuardTests.cs
+++ Guard.Tests/GuardTests.cs
@@ -17,22 +17,76 @@ namespace Guard.Tests
         [Test]
         public void ArgumentNotNull_NullArgument()
         {
-            string invalidString = null;
+            object nullObject = null;
 
             var ex = Assert.Throws<ArgumentNullException>(
-                () => Guard.ArgumentNotNull(invalidString, nameof(invalidString)));
+                () => Guard.ArgumentNotNull(nullObject, nameof(nullObject)));
 
-            StringAssert.Contains(nameof(invalidString), ex.ParamName);
-            StringAssert.Contains($"{nameof(invalidString)} cannot be null", ex.Message);
+            StringAssert.Contains(nameof(nullObject), ex.ParamName);
+            StringAssert.Contains($"{nameof(nullObject)} cannot be null", ex.Message);
         }
 
         [Test]
         public void ArgumentNotNull_NotNullArgument()
         {
-            string validString = "This string is not null.";
+            object notNullObject = new object();
 
             Assert.DoesNotThrow(
-                () => Guard.ArgumentNotNull(validString, nameof(validString)));
+                () => Guard.ArgumentNotNull(notNullObject, nameof(notNullObject)));
+        }
+
+        [Test]
+        public void ArgumentNotNullOrEmptyString_NullString()
+        {
+            string nullString = null;
+
+            var ex = Assert.Throws<ArgumentNullException>(
+                () => Guard.ArgumentNotNullOrEmptyString(nullString, nameof(nullString)));
+
+            StringAssert.Contains(nameof(nullString), ex.ParamName);
+            StringAssert.Contains($"{nameof(nullString)} cannot be null", ex.Message);
+        }
+
+        [Test]
+        public void ArgumentNotNullOrEmptyString_EmptyString()
+        {
+            string emptyString = string.Empty;
+
+            var ex = Assert.Throws<ArgumentException>(
+                () => Guard.ArgumentNotNullOrEmptyString(emptyString, nameof(emptyString)));
+
+            StringAssert.Contains(nameof(emptyString), ex.ParamName);
+            StringAssert.Contains($"{nameof(emptyString)} cannot be empty", ex.Message);
+        }
+
+        [Test]
+        public void ArgumentNotNullOrEmptyString_ValidString()
+        {
+            string validString = "This is a valid string.";
+
+            Assert.DoesNotThrow(
+                () => Guard.ArgumentNotNullOrEmptyString(validString, nameof(validString)));
+        }
+
+        [Test]
+        public void GuidNotEmpty_EmptyGuid()
+        {
+            Guid emptyGuid = Guid.Empty;
+
+            var ex = Assert.Throws<ArgumentException>(
+                () => Guard.GuidNotEmpty(emptyGuid, nameof(emptyGuid)));
+
+            StringAssert.Contains(nameof(emptyGuid), ex.ParamName);
+            StringAssert.Contains($"{nameof(emptyGuid)} cannot be empty", ex.Message);
+        }
+
+        [Test]
+        public void GuidNotEmpty_ValidGuid()
+        {
+            Guid validGuid = Guid.NewGuid();
+
+            Assert.DoesNotThrow(
+                () => Guard.GuidNotEmpty(validGuid, nameof(validGuid)));
         }
     }
 }
--- Guard/Guard.cs
+++ Guard/Guard.cs
@@ -11,11 +11,44 @@ namespace Guard
     /// </summary>
     public static class Guard
     {
+        /// <summary>
+        /// Checks an argument to ensure it isn't null.
+        /// </summary>
+        /// <param name="argumentValue">Specifies the argument value to check.</param>
+        /// <param name="argumentName">Specifies the name of the argument.</param>
         public static void ArgumentNotNull(object argumentValue, string argumentName)
         {
             if (argumentValue == null)
             {
                 throw new ArgumentNullException(argumentName, $"{argumentName} cannot be null");
+            }
+        }
+
+        /// <summary>
+        /// Checks a string argument to ensure it isn't null or empty.
+        /// </summary>
+        /// <param name="argumentValue">Specifies the argument value to check.</param>
+        /// <param name="argumentName">Specifies the name of the argument.</param>
+        public static void ArgumentNotNullOrEmptyString(string argumentValue, string argumentName)
+        {
+            ArgumentNotNull(argumentValue, argumentName);
+
+            if (argumentValue == string.Empty)
+            {
+                throw new ArgumentException($"{argumentName} cannot be empty", argumentName);
+            }
+        }
+
+        /// <summary>
+        /// Checks to ensure that the GUID is not empty.
+        /// </summary>
+        /// <param name="argumentValue">Specifies the argument value to check.</param>
+        /// <param name="argumentName">Specifies the name of the argument.</param>
+        public static void GuidNotEmpty(Guid argumentValue, string argumentName)
+        {
+            if (argumentValue == Guid.Empty)
+            {
+                throw new ArgumentException($"{argumentName} cannot be empty", argumentName);
             }
         }
     }
