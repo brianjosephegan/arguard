c47de9375abb9f4d7908a08f7bea41c1597a3948
Brian Egan <brianjoseph.egan@gmail.com>
2021-01-29 20:34:58 -0800

    Boolean Validation Methods
    Add validation methods and unit tests for verifying boolean arguments.

---
+++ Arguard.Tests/ArguardBooleanTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Arguard.Tests
+{
+    /// <summary>
+    /// Unit tests for <see cref="Arguard"/> class.
+    /// </summary>
+    [TestFixture]
+    public class ArguardBooleanTests
+    {
+        [Test]
+        public void ArgumentTrue_TrueValue()
+        {
+            bool trueArgument = true;
+
+            Assert.DoesNotThrow(
+                () => Arguard.ArgumentTrue(trueArgument, nameof(trueArgument)));
+        }
+
+        [Test]
+        public void ArgumentTrue_FalseValue()
+        {
+            bool falseArgument = false;
+
+            var ex = Assert.Throws<ArgumentException>(
+                () => Arguard.ArgumentTrue(falseArgument, nameof(falseArgument)));
+
+            StringAssert.Contains(nameof(falseArgument), ex.ParamName);
+            StringAssert.Contains($"{nameof(falseArgument)} must be true", ex.Message);
+        }
+
+        [Test]
+        public void ArgumentFalse_FalseValue()
+        {
+            bool falseArgument = false;
+
+            Assert.DoesNotThrow(
+                () => Arguard.ArgumentFalse(falseArgument, nameof(falseArgument)));
+        }
+
+        [Test]
+        public void ArgumentFalse_TrueValue()
+        {
+            bool trueArgument = true;
+
+            var ex = Assert.Throws<ArgumentException>(
+                () => Arguard.ArgumentFalse(trueArgument, nameof(trueArgument)));
+
+            StringAssert.Contains(nameof(trueArgument), ex.ParamName);
+            StringAssert.Contains($"{nameof(trueArgument)} must be false", ex.Message);
+        }
+    }
+}
---
+++ Arguard/ArguardBoolean.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arguard
+{
+    /// <summary>
+    /// Class to provide a range of argument validation methods for IEnumerable scenarios.
+    /// </summary>
+    public static partial class Arguard
+    {
+        /// <summary>
+        /// Checks a boolean argument to ensure it is true.
+        /// </summary>
+        /// <param name="argumentValue">Specifies the argument value to check.</param>
+        /// <param name="argumentName">Specifies the name of the argument.</param>
+        public static void ArgumentTrue(bool argumentValue, string argumentName)
+            => ThrowArgumentExceptionIf(() => argumentValue != true, argumentName, $"{argumentName} must be true");
+
+        /// <summary>
+        /// Checks a boolean argument to ensure it is true.
+        /// </summary>
+        /// <param name="argumentValue">Specifies the argument value to check.</param>
+        /// <param name="argumentName">Specifies the name of the argument.</param>
+        public static void ArgumentFalse(bool argumentValue, string argumentName)
+            => ThrowArgumentExceptionIf(() => argumentValue != false, argumentName, $"{argumentName} must be false");
+    }
+}
